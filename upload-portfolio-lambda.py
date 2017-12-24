aa5cbb916c9b9806a73362d662b71014cec57b3d
Kirk Ferguson <ferguson.kirk@gmail.com>
2017-12-24 15:07:13 -0800

    Updated lambda function to accomodate CodePipeline.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,14 +7,28 @@ def lambda_handler(event, context):
     # Create local object variables
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-west-2:747096364018:deployPortfolioTopic')
+
+    location = {
+      'bucketName': 'portfoliobuild.kirkferguson.info',
+      'objectKey': 'portfoliobuild.zip'
+    }
     try:
+      job = event.get('CodePipeline.job')
+
+      if job:
+        for artifact in job['data']['inputArtifacts']:
+          if artifact['name'] == 'MyAppBuild':
+            location = artifact['location']['s3Location']
+
+      print 'Building portfolio from ' + str(location)
+
       s3 = boto3.resource('s3')
       portfolio_bucket = s3.Bucket('portfolio.kirkferguson.info')
-      build_bucket = s3.Bucket('portfoliobuild.kirkferguson.info')
+      build_bucket = s3.Bucket(location['bucketName'])
       portfolio_zip = StringIO.StringIO()
 
       # Grab the zip file CodeBuild created from GitHub
-      build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+      build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
       # For each file in the zip, move the file to the PRD s3 bucket and grant
       # public read priviledge
@@ -27,6 +41,10 @@ def lambda_handler(event, context):
 
       print 'Files moved from build bucket to PRD.'
       topic.publish(Subject='Files Deployed to Portfolio', Message='Success.')
+
+      if job:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_success_result(jobId=job['id'])
     except:
       topic.publish(Subject='Files Failed to Deploy to Portfolio', Message='Failed.')
       raise
