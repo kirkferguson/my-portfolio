bdf5ead8b61f7bc5c7b709f18df823f88cec038d
Kirk Ferguson <ferguson.kirk@gmail.com>
2017-12-24 07:19:47 -0800

    Added exception handling to lambda function.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,23 +5,30 @@ import mimetypes
 
 def lambda_handler(event, context):
     # Create local object variables
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.kirkferguson.info')
-    build_bucket = s3.Bucket('portfoliobuild.kirkferguson.info')
-    portfolio_zip = StringIO.StringIO()
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-west-2:747096364018:deployPortfolioTopic')
+    try:
+      s3 = boto3.resource('s3')
+      portfolio_bucket = s3.Bucket('portfolio.kirkferguson.info')
+      build_bucket = s3.Bucket('portfoliobuild.kirkferguson.info')
+      portfolio_zip = StringIO.StringIO()
 
-    # Grab the zip file CodeBuild created from GitHub
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+      # Grab the zip file CodeBuild created from GitHub
+      build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    # For each file in the zip, move the file to the PRD s3 bucket and grant
-    # public read priviledge
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-      for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+      # For each file in the zip, move the file to the PRD s3 bucket and grant
+      # public read priviledge
+      with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+          obj = myzip.open(nm)
+          portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+          portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    print 'Files moved from build bucket to PRD.'
+      print 'Files moved from build bucket to PRD.'
+      topic.publish(Subject='Files Deployed to Portfolio', Message='Success.')
+    except:
+      topic.publish(Subject='Files Failed to Deploy to Portfolio', Message='Failed.')
+      raise
 
-    return 'Success.'
+    return 'Lambda execution complete.'
