f41e200de23401748e2f27c39379f38dec88f887
Kirk Ferguson <ferguson.kirk@gmail.com>
2017-12-25 15:34:39 -0800

    Added react example work component.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Director, IT",
+    'image': {
+      'desc': "Example screenshot of a project involving code.",
+      'src': "images/code.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "AWS Certified",
+    'image': {
+      'desc': "Screenshot of AWS Solutions Architect Associate badge.",
+      'src': "images/awssaa.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "United States Navy Veteran",
+    'image': {
+      'desc': "Screenshot of US Naval Special Warfare Officer.",
+      'src': "images/USNavy.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "ReactJS / AWS Serverless Website",
+    'image': {
+      'desc': "Screenshot of the architecture diagram for this site.",
+      'src': "images/port-arch-tn.png",
+      'comment': ""
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
